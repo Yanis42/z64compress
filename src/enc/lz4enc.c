f7e0a831248404ee4d1181f935ebe466e9c98643
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2023-08-01 01:36:51 +0200

    progress

--- src/enc/lz4enc.c
+++ src/enc/lz4enc.c
@@ -3,54 +3,44 @@
 #include <string.h>
 #include <stdbool.h>
 #include <assert.h>
+#include <stdio.h>
 
 #define LZ4_DISTANCE_MAX 16384
-#include "./lz4/lz4hc.h"
+#include "lz4/lz4hc.h"
 
-static bool includeHeader = true;
 
 int lz4enc(void *_src, unsigned src_sz, void *_dst, unsigned *dst_sz, void *_ctx) {
 	char *src = _src;
 	char *dst = _dst;
 	int result_sz = 0;
-	int compress_bound = 0;
-	short lenHeader = 0;
+	extern int g_hlen; /* header length */
 
 	/* build header */
-	if (includeHeader)
-	{
-		extern int g_hlen; /* header length */
-		short lenHeader = g_hlen + 4 + 4;
-		memset(dst, 0, lenHeader);
-		memcpy(dst, "LZ40", 4);
+	memset(dst, 0, g_hlen);
+	memcpy(dst, "LZ40", 4);
 
-		// save src_sz
-		dst[4] = (src_sz >> 24);
-		dst[5] = (src_sz >> 16);
-		dst[6] = (src_sz >> 8);
-		dst[7] = (src_sz >> 0);
-	}
+	// save decompressed size
+	dst[4] = (src_sz >> 24);
+	dst[5] = (src_sz >> 16);
+	dst[6] = (src_sz >> 8);
+	dst[7] = (src_sz >> 0);
 
 	/* compress */
-	compress_bound = LZ4_COMPRESSBOUND(src_sz);
-	result_sz = LZ4_compress_HC(src, dst + lenHeader, src_sz, compress_bound, LZ4HC_CLEVEL_MAX);
-	assert(result_sz > 0);
-	assert(result_sz <= compress_bound);
+	int compress_bound = LZ4_COMPRESSBOUND(src_sz) - g_hlen;
+	dst += g_hlen;
 
-	/* save compressed size in the header and align it */
-	if (includeHeader)
-	{
-		// save result_sz
-		dst[8] = (result_sz >> 24);
-		dst[9] = (result_sz >> 16);
-		dst[10] = (result_sz >> 8);
-		dst[11] = (result_sz >> 0);
-
-		// align
-		dst[12] = dst[13] = dst[14] = dst[15] = 0;
+	if (!compress_bound) {
+		fprintf(stderr, "error with compression bound\n");
+		return 1;
 	}
 
-	/* update the compressed size parameter*/
-	*dst_sz = result_sz + lenHeader;
+	result_sz = LZ4_compress_HC(src, dst, src_sz, compress_bound, LZ4HC_CLEVEL_MAX);
+	if (!result_sz || !(result_sz <= compress_bound)) {
+		fprintf(stderr, "error with result (0x%08X, 0x%08X)\n", result_sz, compress_bound);
+		return 1;
+	}
+
+	/* update the compressed size parameter */
+	*dst_sz = result_sz + g_hlen;
 	return 0;
 }
